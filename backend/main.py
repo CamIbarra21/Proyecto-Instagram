d523c5487d8bab36161b9ce9174aaa80cb2400ff
Camila Ibarra <cami05aic@gmail.com>
2025-12-22 21:47:44 -0500

    feat: configuraciones para el deploy

--- backend/main.py
+++ backend/main.py
@@ -28,10 +28,11 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 # --- 3. CONFIGURACIÓN BASE DE DATOS ---
 def get_db_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="", # Por defecto en XAMPP es vacío
-        database="proyecto_insta_db"
+        host=os.getenv("DB_HOST", "localhost"),
+        user=os.getenv("DB_USER", "root"),
+        password=os.getenv("DB_PASSWORD", ""),
+        database=os.getenv("DB_NAME", "proyecto_insta_db"),
+        port=int(os.getenv("DB_PORT", 3306))
     )
 
 # --- 4. CONFIGURACIÓN IA (Zero-Shot) ---
